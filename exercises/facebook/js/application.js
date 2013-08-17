658a06edc1562d6035872b122d223b706e0b142b
rocketu <klee@rocket-space.com>
2013-08-16 18:06:19 -0700

    FB Permissions

--- exercises/facebook/js/application.js
+++ exercises/facebook/js/application.js
@@ -11,7 +11,7 @@ window.fbAsyncInit = function() {
 	// Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
 	// for any authentication related change, such as login, logout or session refresh. This means that
 	// whenever someone who was previously logged out tries to log in again, the correct case below 
-	// will be handled. 
+	// will be handled.
 	FB.Event.subscribe('auth.authResponseChange', function(response) {
 		console.log(response);
 		// Here we specify what we do with the response anytime this event occurs. 
@@ -19,6 +19,7 @@ window.fbAsyncInit = function() {
 			// The response object is returned with a status field that lets the app know the current
 			// login status of the person. In this case, we're handling the situation where they 
 			// have logged in to the app.
+			alreadyConnected = true;				
 			fbAccessToken = response.authResponse.accessToken;
 			login();
 		} else if (response.status === 'not_authorized') {
@@ -29,14 +30,18 @@ window.fbAsyncInit = function() {
 			// (1) JavaScript created popup windows are blocked by most browsers unless they 
 			// result from direct interaction from people using the app (such as a mouse click)
 			// (2) it is a bad experience to be continually prompted to login upon page load.
-			FB.login();
+			FB.login(function(response) {
+				
+			},{ scope: 'email,user_likes,friend_likes' });
 		} else {
 			// In this case, the person is not logged into Facebook, so we call the login() 
 			// function to prompt them to do so. Note that at this stage there is no indication
 			// of whether they are logged into the app. If they aren't then they'll see the Login
 			// dialog right after they log in to Facebook. 
 			// The same caveats as above apply to the FB.login() call here.
-			FB.login();
+			FB.login(function(response) {
+				
+			},{ scope: 'email,user_likes,friend_likes,user_education_history' });
 		}
 	});
 };
@@ -79,6 +84,9 @@ function getMe() {
 		imgSrc = 'https://graph.facebook.com/' + response.username + '/picture?type=large';
 		$('body').append( $('<h1/>').html(name) );
 		$('body').append( $('<img/>').attr('src',imgSrc) );
+	});	
+	FB.api('/me/permissions', function(response) {
+		console.log(response);
 	});	
 }
 
